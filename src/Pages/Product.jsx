cd197a4a3ed1aa36fdd5c3574f8daf21b9ff2a0a
devtushar001 <tc341269@gmail.com>
2024-10-22 03:37:22 -0500

    login signup page created successfully

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,5 +1,11 @@
+import React, { useContext } from "react";
+import ShopContext from '../Context/ShopContext'
+import { useParams } from "react-router-dom";
 
 const Product = () => {
+    const {all_products} = useContext(ShopContext);
+    const {ProductId} = useParams();
+    const product = all_products.find((e) => e.id === Number(ProductId));
     return (
         <>
           
